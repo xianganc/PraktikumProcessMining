a1cbef183ba12d21ca87193ac13cadc04761650b
Martin Hashem <martin.hashem@rwth-aachen.de>
2019-05-20 16:23:25 +0200

    adding some stuff

---
+++ integration/deps/init.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl http://mirror.dkd.de/apache/hadoop/common/hadoop-3.2.0/hadoop-3.2.0.tar.gz -o hadoop.tar.gz ;
+tar -xzf  hadoop.tar.gz ;
+mv hadoop-3.2.0 hadoop ;
--- startup.sh
+++ startup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-sudo apt-get install curl
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-get install -y curl ;
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - ;
 sudo add-apt-repository \
   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) \
-  stable"
-sudo apt-get update
+  stable" ;
+sudo apt-get update ;
 sudo apt-get install -y \
   python3.6 \
   python3-pip \
@@ -19,12 +19,10 @@ sudo apt-get install -y \
   docker-ce-cli \
   containerd.io
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose ;
+sudo chmod +x /usr/local/bin/docker-compose ;
 
-git clone https://github.com/xianganc/PraktikumProcessMining.git
-cd PraktikumProcessMining/integration
-curl http://mirror.dkd.de/apache/hadoop/common/hadoop-3.2.0/hadoop-3.2.0.tar.gz -o hadoop.tar.gz
-tar -xzf  hadoop.tar.gz
-mv hadoop-3.2.0 hadoop
-git checkout integration
+git clone https://github.com/xianganc/PraktikumProcessMining.git ;
+cd PraktikumProcessMining/integration ;
+
+git checkout integration ;
