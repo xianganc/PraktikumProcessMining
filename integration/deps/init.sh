2e597259759e7bd539e33524d0e980ea826d3187
Martin Hashem <martin.hashem@rwth-aachen.de>
2019-05-20 17:09:35 +0200

    adding

--- integration/deps/init.sh
+++ integration/deps/init.sh
@@ -3,5 +3,6 @@
 curl http://mirror.dkd.de/apache/hadoop/common/hadoop-3.2.0/hadoop-3.2.0.tar.gz -o hadoop.tar.gz ;
 tar -xzf  hadoop.tar.gz ;
 mv hadoop-3.2.0 hadoop ;
+rm hadoop.tar.gz ;
 
 trap terminate 2 15
