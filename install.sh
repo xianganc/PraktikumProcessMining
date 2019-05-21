15a366d0085fc449df4423e314ad232a568b26df
Martin Hashem <martin.hashem@rwth-aachen.de>
2019-05-21 15:23:16 +0200

    adding dep

--- install.sh
+++ install.sh
@@ -16,7 +16,8 @@ sudo apt-get install -y \
   docker-ce \
   docker-ce-cli \
   containerd.io \
-  default-jre
+  default-jre \
+  ufw
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose ;
 sudo chmod +x /usr/local/bin/docker-compose ;
