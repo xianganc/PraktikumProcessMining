c033faa5a186ff00cf3e4b812f54738d19ef3b37
Martin Hashem <martin.hashem@rwth-aachen.de>
2019-05-21 12:07:18 +0200

    changes

--- integration/deps/init.sh
+++ integration/deps/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-adduser --system --shell /bin/bash --group --disabled-password hadoop
+adduser -p hadoop hadoop
 usermod -aG sudo hadoop
 curl http://mirror.dkd.de/apache/hadoop/common/hadoop-3.2.0/hadoop-3.2.0.tar.gz -o hadoop.tar.gz ;
 tar -xzf  hadoop.tar.gz ;
@@ -10,8 +10,8 @@ rm hadoop.tar.gz ;
 python3 /src/hadoop.py
 mkdir -p /home/hadoop/.ssh/
 chown hadoop:hadoop /home/hadoop/.ssh/
-ssh-keygen -t rsa -N "" -f /home/hadoop/.ssh/id_rsa
-runuser -l hadoop -c 'ssh-copy-id -i ~/.ssh/id_rsa.pub hadoop@node-master'
+runuser -l hadoop -c 'ssh-keygen -t rsa -N "" -f /home/hadoop/.ssh/id_rsa'
+echo 'hadoop' | runuser -l hadoop -c 'ssh-copy-id -i ~/.ssh/id_rsa.pub hadoop@node-master'
 /hadoop/bin/hdfs namenode -format
 start_hadoop
 
