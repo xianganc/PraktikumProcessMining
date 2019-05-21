6a1fad801a5b194af73abb012d416073b2e8c366
Martin Hashem <martin.hashem@rwth-aachen.de>
2019-05-21 13:04:24 +0200

    rm

--- integration/deps/init.sh
+++ integration/deps/init.sh
@@ -10,8 +10,6 @@ rm hadoop.tar.gz ;
 python3 /src/hadoop.py
 mkdir -p /home/hadoop/.ssh/
 chown -R hadoop:hadoop /home/hadoop/.ssh/
-runuser -u hadoop -c 'ssh-keygen -b 4096 -N "" -f /home/hadoop/.ssh/id_rsa'
-
 /hadoop/bin/hdfs namenode -format
 start_hadoop
 
