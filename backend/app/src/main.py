7cad921cd6fccda192efc737e06836f416b8e12e
Martin Hashem <martin.hashem@rwth-aachen.de>
2019-06-07 15:14:36 +0200

    fixing paths

--- backend/app/src/main.py
+++ backend/app/src/main.py
@@ -2,7 +2,7 @@ from hadoop import HadoopInteractions
 import time
 
 h = HadoopInteractions()
-h.pushData('/src/bin/','/.')
-h.showData('/.')
+h.pushData('/src/bin/','/')
+h.showData('/')
 while True:
   time.sleep(10)
