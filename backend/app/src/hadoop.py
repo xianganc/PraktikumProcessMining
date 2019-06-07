4ad965abd83af43b7884db7f803737f881a2a1af
Martin Hashem <martin.hashem@rwth-aachen.de>
2019-06-07 19:06:33 +0200

    reverting a lot

--- backend/app/src/hadoop.py
+++ backend/app/src/hadoop.py
@@ -7,18 +7,18 @@ class HadoopInteractions:
 
   def pushData(self,from_,to_):
     """ pushing data into hadoop """
-    subprocess.check_output(["ssh","ubuntu@prak.hashtagm.de",'~/PraktikumProcessMining/hadoop/bin/hdfs dfs -mkdir -p '+ to_])
-    subprocess.check_output(["ssh","ubuntu@prak.hashtagm.de",'~/PraktikumProcessMining/hadoop/bin/hdfs dfs -put '+from_ + ' '+ to_])
+    subprocess.check_output(["/src/hadoop/bin/hdfs","dfs","-mkdir","-p",to_])
+    subprocess.check_output(["/src/hadoop/bin/hdfs","dfs","-put",from_,to_])
     pass
 
   def getData(self, from_, to_):
     """ get data from hadoop """
-    subprocess.check_output(["ssh","ubuntu@prak.hashtagm.de",'~/PraktikumProcessMining/hadoop/bin/hdfs dfs -get '+ from_ + ' '+ to_])
+    subprocess.check_output(["/src/hadoop/bin/hdfs", "dfs", "-get", from_,to_])
     pass
 
   def showData(self,from_):
     """ get data from hadoop """
-    subprocess.check_output(["ssh","ubuntu@prak.hashtagm.de",'~/PraktikumProcessMining/hadoop/bin/hdfs dfs -ls -R %s' % from_])
+    subprocess.check_output(["/src/hadoop/bin/hdfs", "dfs", "-ls", "-R", from_])
     pass
 
   def runMR(self, data):
