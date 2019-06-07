ab937427001bc7f908ac176eb41eaf09995d9a5b
Martin Hashem <martin.hashem@rwth-aachen.de>
2019-06-07 15:27:14 +0200

    removing binaries

--- backend/app/src/hadoop.py
+++ backend/app/src/hadoop.py
@@ -7,17 +7,18 @@ class HadoopInteractions:
 
   def pushData(self,from_,to_):
     """ pushing data into hadoop """
-    subprocess.check_output(["hadoop_push",from_,to_])
+    subprocess.check_output(["runuser","hadoop","-c" ,'/src/bin/hdfs -mkdir '+ to_])
+    subprocess.check_output(["runuser","hadoop","-c" ,'/src/bin/hdfs -put '+from_ + ' '+ to_])
     pass
 
   def getData(self, from_, to_):
     """ get data from hadoop """
-    subprocess.check_output(["hadoop_pull",from_,to_])
+    subprocess.check_output(["runuser","hadoop","-c" ,'/src/bin/hdfs -get '+ from_ + ' '+ to_])
     pass
 
   def showData(self,from_):
     """ get data from hadoop """
-    subprocess.check_output(["hadoop_show",from_])
+    subprocess.check_output(["runuser","hadoop","-c" ,'/src/bin/hdfs -ls -R '+ from_])
     pass
 
   def runMR(self, data):
