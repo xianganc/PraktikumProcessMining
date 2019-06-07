abc5471c6e084262ad8d3a4b57c311494db35a14
Martin Hashem <martin.hashem@rwth-aachen.de>
2019-06-07 09:36:58 +0200

    adding outline for hadoop

---
+++ backend/app/src/hadoop.py
@@ -0,0 +1,23 @@
+
+
+class HadoopInteractions:
+  def __init__(self):
+    """creating Interface"""
+    pass
+
+  def pushData(self,dataPath):
+    """ pushing data into hadoop """
+    pass
+
+  def getData(self, data):
+    """ get data from hadoop """
+    pass
+
+  def runMR(self, data):
+    """ running the Map-Reduce Job """
+    pass
+
+  def runAlpha(self, data):
+    """ running the alpha-algorithm """
+    pass
+
--- backend/deps/deps.sh
+++ backend/deps/deps.sh
@@ -4,3 +4,5 @@ apt-get install -y \
   python3.6 \
   python3-pip \
   openssh-server
+
+pip3 install pm4py
