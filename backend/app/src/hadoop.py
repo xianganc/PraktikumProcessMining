5b1ef31513fcf4b618e7e4ab90424dff8aa050b1
Martin Hashem <martin.hashem@rwth-aachen.de>
2019-06-07 17:19:06 +0200

    paths

--- backend/app/src/hadoop.py
+++ backend/app/src/hadoop.py
@@ -7,18 +7,18 @@ class HadoopInteractions:
 
   def pushData(self,from_,to_):
     """ pushing data into hadoop """
-    subprocess.check_output(["runuser","hadoop","-c" ,'/src/bin/hdfs dfs -mkdir '+ to_])
-    subprocess.check_output(["runuser","hadoop","-c" ,'/src/bin/hdfs dsf -put '+from_ + ' '+ to_])
+    subprocess.check_output(["runuser","hadoop","-c" ,'/src/hadoop/bin/hdfs dfs -mkdir '+ to_])
+    subprocess.check_output(["runuser","hadoop","-c" ,'/src/hadoop/bin/hdfs dsf -put '+from_ + ' '+ to_])
     pass
 
   def getData(self, from_, to_):
     """ get data from hadoop """
-    subprocess.check_output(["runuser","hadoop","-c" ,'/src/bin/hdfs dfs -get '+ from_ + ' '+ to_])
+    subprocess.check_output(["runuser","hadoop","-c" ,'/src/hadoop/bin/hdfs dfs -get '+ from_ + ' '+ to_])
     pass
 
   def showData(self,from_):
     """ get data from hadoop """
-    subprocess.check_output(["runuser","hadoop","-c" ,'/src/bin/hdfs dfs -ls -R '+ from_])
+    subprocess.check_output(["runuser","hadoop","-c" ,'/src/hadoop/bin/hdfs dfs -ls -R '+ from_])
     pass
 
   def runMR(self, data):
