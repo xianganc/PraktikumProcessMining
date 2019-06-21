8fc3f078d0b74914709891116934afcc995c3307
Martin Hashem <martin.hashem@rwth-aachen.de>
2019-06-21 17:10:47 +0200

    adding a path

--- backend/app/src/hadoop.py
+++ backend/app/src/hadoop.py
@@ -7,19 +7,19 @@ class HadoopInteractions:
 
   def pushData(self,from_,to_):
     """ pushing data into hadoop """
-    subprocess.check_output(["/src/bin/hdfs","dfs","-mkdir","-p",to_])
-    subprocess.check_output(["/src/bin/hdfs","dfs","-put",from_,to_])
+    subprocess.check_output(["/src/bin/hdfs","dfs","-mkdir","-p","/home/"+to_])
+    subprocess.check_output(["/src/bin/hdfs","dfs","-put",from_,"/home/"+to_])
     subprocess.check_output(["rm", "-f",from_])
     pass
 
   def getData(self, from_, to_):
     """ get data from hadoop """
-    subprocess.check_output(["/src/bin/hdfs", "dfs", "-get", from_,to_])
+    subprocess.check_output(["/src/bin/hdfs", "dfs", "-get", from_,"/home/"+to_])
     pass
 
   def showData(self,from_):
     """ get data from hadoop """
-    out = subprocess.check_output(["/src/bin/hdfs", "dfs", "-ls", "-R", from_])
+    out = subprocess.check_output(["/src/bin/hdfs", "dfs", "-ls", "-R", "/home/"+from_])
     res = []
     for line in out:
       res.append(line)
