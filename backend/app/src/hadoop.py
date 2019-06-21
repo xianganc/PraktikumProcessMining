01e53eb36e905bd693ba5c3fdb754dd6cf3d8f0e
Martin Hashem <martin.hashem@rwth-aachen.de>
2019-06-21 17:32:09 +0200

    a

--- backend/app/src/hadoop.py
+++ backend/app/src/hadoop.py
@@ -20,7 +20,7 @@ class HadoopInteractions:
   def showData(self,from_):
     """ get data from hadoop """
     out = subprocess.check_output(["/src/bin/hdfs", "dfs", "-ls", "-R", "/home"+from_])
-    print(out)
+    print("SHOW DATA FOR STUFF "+out)
     res = []
     for line in out:
       res.append(line.split()[-1])
