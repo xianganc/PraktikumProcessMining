3320c1777deb4c7b6f8b938e78134ee879730f17
Martin Hashem <martin.hashem@rwth-aachen.de>
2019-07-01 11:34:23 +0200

    some fixes

--- backend/app/src/alphaAlgo.py
+++ backend/app/src/alphaAlgo.py
@@ -2,7 +2,7 @@ from pm4py.algo.discovery.alpha import factory as alpha_miner
 from pm4py.objects.log.importer.xes import factory as xes_importer
 from pm4py.visualization.petrinet import factory as vis_factory
 
-log = xes_importer.import_log('<path-to-xes-log-file>')
+log = xes_importer.import_log('/data/output/example.xes')
 net, initial_marking, final_marking = alpha_miner.apply(log)
 gviz = vis_factory.apply(net, initial_marking, final_marking)
-vis_factory.view(gviz)
+vis_factory.save(gviz,'output.png')
