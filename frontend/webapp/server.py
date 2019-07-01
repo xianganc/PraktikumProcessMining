5059eb87100f328cdc947527b36a61a407e5e61b
Martin Hashem <martin.hashem@rwth-aachen.de>
2019-07-01 17:13:07 +0200

    logging and paths

--- frontend/webapp/server.py
+++ frontend/webapp/server.py
@@ -59,9 +59,10 @@ def return_img_stream(img_local_path):
 
 @app.route('/result')
 def show():
-	p = subprocess.call(['curl', '-s', '172.18.0.1:3000/api/alpha'])
+	p = subprocess.check_output(['curl', '-s', '172.18.0.1:3000/api/alpha'])
 	print("show alpha")
-	os.rename("/data/output.png","static/outputs/output.png")
+	app.logger.info(p.communicate())
+	os.rename("/data/output.png","/var/www/static/outputs/output.png")
 	img_path = 'static/outputs/output.png'
 	img_stream = return_img_stream(img_path)
 	return render_template('result.html',img_stream=img_stream)
