a73875038980876989eae575167f4deefdabfa4d
Martin Hashem <martin.hashem@rwth-aachen.de>
2019-07-07 16:31:15 +0200

    reduce?

--- backend/app/src/reducer.py
+++ backend/app/src/reducer.py
@@ -3,8 +3,15 @@ class Reduce:
   def __init__(self):
     pass
   def reduce1(self, logDict):
+    resDict = {}
+    tmp = set()
     for key in logDict.keys():
-      print(sorted(logDict[key],key=lambda k:k[1]))
-
+      trace = sorted(logDict[key],key=lambda k:k[1])
+      for i in trace:
+        for j in trace:
+          if i == j:
+            continue
+          tmp.add(((trace[i],trace[j]),True))
+    print(tmp)
   def reduce2(self):
     pass
