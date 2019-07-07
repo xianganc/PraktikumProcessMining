c4bada93f233c14264723a4ce11769be276973d8
Martin Hashem <martin.hashem@rwth-aachen.de>
2019-07-07 16:09:43 +0200

    testing

--- backend/app/src/reducer.py
+++ backend/app/src/reducer.py
@@ -2,7 +2,9 @@
 class Reduce:
   def __init__(self):
     pass
-  def reduce1(self, dic):
-    pass
+  def reduce1(self, logDict):
+    for key in logDict.keys():
+      print(sorted(logDict[key],key=lambda k:k[1]))
+
   def reduce2(self):
     pass
