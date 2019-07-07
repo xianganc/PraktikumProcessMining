5d6422a8163d472265b5caa865bc2e5194ca4537
Martin Hashem <martin.hashem@rwth-aachen.de>
2019-07-07 16:46:44 +0200

    reducer done?

--- backend/app/src/reducer.py
+++ backend/app/src/reducer.py
@@ -1,17 +1,27 @@
+import json
 
 class Reduce:
   def __init__(self):
     pass
-  def reduce1(self, logDict):
+
+def reduce1(self, logDict):
     resDict = {}
-    tmp = set()
+    tmp = {}
     for key in logDict.keys():
       trace = sorted(logDict[key],key=lambda k:k[1])
-      for i in trace:
-        for j in trace:
+      for i in range(len(trace)):
+        for j in range(len(trace)):
           if i == j:
             continue
-          tmp.add(((trace[i],trace[j]),True))
-    print(tmp)
+          elif i < j:
+            if (trace[j][0],trace[i][0]) not in tmp:
+              tmp[(trace[j][0],trace[i][0])] = (False,False)
+            tmp[(trace[j][0],trace[i][0])] = (False or tmp[(trace[j][0],trace[i][0])][0],tmp[(trace[j][0],trace[i][0])][1] or True)
+          else:
+            if (trace[j][0],trace[i][0]) not in tmp:
+              tmp[(trace[j][0],trace[i][0])] = (False,False)
+            tmp[(trace[j][0],trace[i][0])] = (True or tmp[(trace[j][0],trace[i][0])][0],tmp[(trace[j][0],trace[i][0])][1] or False)
+    return tmp
+
   def reduce2(self):
     pass
