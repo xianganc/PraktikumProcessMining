4c525fda4abd2f37f223f821a6902198ee932422
Martin Hashem <martin.hashem@rwth-aachen.de>
2019-07-09 14:27:19 +0200

    updating test

--- backend/app/src/test.py
+++ backend/app/src/test.py
@@ -1,10 +1,36 @@
 import json
 import mapper
 import reducer
+from alpha import Alpha
+from petri_net import PetriNet
+from subprocess import check_call
 
-mp = mapper.Mapper()
-rp = reducer.Reduce()
+def test1():
+  mp = mapper.Mapper()
+  rp = reducer.Reduce()
 
-tmp = mp.map1Csv('/home/ichus/Documents/PraktikumProcessMining/data/running-example.csv',activity='Activity',timestamp='time:timestamp',case='case:concept:name')
-tmp = rp.reduce1(tmp)
-print(rp.reduce1(tmp))
+  tmp = mp.map1Csv('/home/ichus/Documents/PraktikumProcessMining/data/running-example.csv',activity='Activity',timestamp='time:timestamp',case='case:concept:name')
+  tmp = rp.reduce1(tmp)
+  res = rp.reduce2(tmp)
+  print(res)
+  with open('reduced.json') as reducedLog:
+    json.dump(res,reducedLog)
+
+def test2(infile):
+
+  alpha_model = Alpha(infile)
+  print('init done')
+  alpha_model.generate_footprint(txtfile="{}_footprint.txt".format(infile))
+  print("footprint done")
+  pn = PetriNet()
+  print("petrinet init done")
+  pn.generate_with_alpha(alpha_model, dotfile="{}.dot".format(infile))
+  print("petrinet done")
+  check_call(["dot", "-Tpng", "{}.dot".format(infile),"-o", "{}.png".format(infile)])
+  print("viz done")
+
+if __name__ == '__main__':
+  test1()
+  print('test1 done')
+  test2('reduced.json')
+  print("test2 done")
